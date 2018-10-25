283b31bbdebd2bfc7742a0b19b530593864a91f0
mh2k9 <deev2k9@gmail.com>
2018-10-25 12:46:59 +0600

    Create simple-encryption-1-alternating-split.php

---
+++ simple-encryption-1-alternating-split.php
@@ -0,0 +1,26 @@
+<?php
+function encrypt($text, $n) {
+    return encryptDecrypt($text, $n);
+}
+
+function decrypt($text, $n) {
+    return encryptDecrypt($text, $n);
+}
+
+function encryptDecrypt($text , $n) {
+    if ($n < 1) {
+        return $text;
+    }
+
+    while ($n > 0) {
+        $array = str_split($text);
+        $odd = $even = [];
+        $both = [&$even, &$odd];
+        array_walk($array, function($v, $k) use ($both) { $both[$k % 2][] = $v; });
+        $text = implode('', $both[1]) . implode('', $both[0]);
+        $odd = $even = $both = $array = [];
+        $n--;
+    }
+
+    return $text;
+}
